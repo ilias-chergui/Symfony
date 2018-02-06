fb92ae84328f57a6e7495f09d56596c4caca9232
ilias-chergui <chergui.ilias@gmail.com>
2018-02-06 11:51:47 +0100

    Symfony's basics chap1 done

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -16,9 +16,24 @@ class AdvertController extends Controller
   function indexAction()
   {
     // On utilise une variable qui hérite du controller
-    $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig');
+    $content = $this
+      ->get('templating')
+      ->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Winzou' ))
+    ;
 
     // On crée un réponse contenant une variable
+    return new Response($content);
+  }
+
+  function byeAction()
+  {
+    $content = $this
+      ->get('templating')
+      ->render('OCPlatformBundle:Advert:bye.html.twig',
+        array('aurevoir' => 'Tchouuuus',
+              'nom' => 'Weichuan'
+        )
+      );
     return new Response($content);
   }
 }
