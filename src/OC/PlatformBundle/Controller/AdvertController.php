cea25f9a2e1d363ad698c3adc8c24937ba7e1ca8
ilias-chergui <chergui.ilias@gmail.com>
2018-02-06 16:19:31 +0100

    half chap 2

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -11,6 +11,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdvertController extends Controller
 {
+  // On donne à la methode l'argument $id
+  // Qui va correspondre à {id} de la route (URL)
+  function viewAction($id)
+  {
+    return new Response ("Affichage de l'annonce d'id : ".$id);
+  }
+
+  function viewSlugAction($slug, $year, $format)
+  {
+    return new Response ("On pourra afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$format.".");
+  }
 
   // On définit la méthode d'index (Ps: toujours ajouter "Action")
   function indexAction()
